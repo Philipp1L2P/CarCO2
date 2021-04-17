e26986c33a6fdd161e239d1f2a1140a6b346be70
mastercam123 <k4mbingoce@yahoo.com>
2021-04-17 13:24:12 +0200

    ## Text wurde von Husain geändert

--- mysite/carco2/views.py
+++ mysite/carco2/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. CarCO2 lives")
+    return HttpResponse("Ahoi, hier ist die Benutzerbedienungoberfläche im Django")
